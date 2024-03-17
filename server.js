f73925cfe9ff14d4dcda04062713daa359ef1b8b
abenet-asnake <abenet.asnaketesfaye@gmail.com>
2024-03-17 10:48:02 +0300

    server is init

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ const express= require('express');
 
 const app = express();
 
-const 
+ const port= process.env.PORT || 4000;
+
+ app.listen(port,()=>{
+    console.log(`Server listening on ${port}`);
+ });
